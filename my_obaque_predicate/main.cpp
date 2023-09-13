6b13380e6c6bc3f3cfefe14a2c6dcfb3f17a35b6
helloobaby <sun888486@qq.com>
2023-09-13 12:40:11 +0800

    update

---
+++ my_obaque_predicate/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int a=0;
+    cin >> a;
+
+    if(a>2 && a <1){
+        printf("is branch reachable?\n");
+    }
+
+
+    if(a == 5){
+        printf("is branch reachable?\n");
+    }
+
+    return 0;
+}
